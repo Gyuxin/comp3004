3d0373d4a41bcaf9380d731f3bcee2f3ce856eef
GaoYuxin <gaomnemosyne@Gmail.com>
2018-09-15 16:34:56 -0400

    Write test case and Card.java according to CardTest.java. Haven't test case 'ACE' yet.

--- src/main/java/core/Card.java
+++ src/main/java/core/Card.java
@@ -1,5 +1,24 @@
 package core;
 
+//a CARD
 public class Card {
+private String score;
+
+public Card(String s){
+ this.score = s;
+}
+
+public int getScore(){
+
+ String word = score.substring(1);
+ if(word.equals('A')){
+   return 11;
+ } else if (word.equals("J") || word.equals("Q") || word.equals("K")){
+   return 10;
+ } else {
+   int num = Integer.parseInt(word);
+   return num;
+ }
+}
 
 }
--- src/test/java/core/CardTest.java
+++ src/test/java/core/CardTest.java
@@ -4,8 +4,20 @@ import junit.framework.TestCase;
 
 public class CardTest extends TestCase{
 	
-	public void testTheScoreOfCards(){
-		
+	public void testTheScoreOfJQKCards(){
+		Card jack = new Card("DJ");
+		Card queen = new Card("HQ");
+		Card king = new Card("SK");
+		assertEquals(10, jack.getScore());
+		assertEquals(10, queen.getScore());
+		assertEquals(10, king.getScore());
+	}
+	
+	public void testTheScoreOfNumCards(){
+		Card card1 = new Card("C2");
+		Card card2 = new Card("C10");
+		assertEquals(2, card1.getScore());
+		assertEquals(10, card2.getScore());
 	}
 
 }
