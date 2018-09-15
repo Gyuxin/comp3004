775a89ef7d465f968fca0b1fde360b721b224b39
GaoYuxin <gaomnemosyne@Gmail.com>
2018-09-15 14:30:22 -0400

    Starting with CardTest.java. testing card.getRecord() method

---
+++ src/test/java/core/CardTest.java
@@ -0,0 +1,11 @@
+package core;
+
+import junit.framework.TestCase;
+
+public class CardTest extends TestCase{
+	
+	public void testTheScoreOfCards(){
+		
+	}
+
+}
---
+++ src/test/java/core/PokerTest.java
@@ -0,0 +1,15 @@
+package core;
+
+import junit.framework.TestCase;
+
+public class PokerTest extends TestCase{
+	
+	public void testNumberOfCards(){
+		
+	}
+	
+	public void testNumberOfCardsWhenRemoveOneCard(){
+		
+	}
+	
+}
